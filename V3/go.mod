aa1757a0dfd81033efa3ddaa8a530cf3ec7efa31
JhonatanRSantos <jhonatanatm@gmail.com>
2021-08-15 15:18:33 -0300

    Structs and Pointers
    - Added a sample of how use structs and pointers

---
+++ V3/go.mod
@@ -0,0 +1,3 @@
+module github.com/JhonatanRSantos/go-learning/V3
+
+go 1.16
---
+++ V3/interfaces/person.go
@@ -0,0 +1,31 @@
+package interfaces
+
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+	Contact
+	Values []string
+}
+
+type Contact struct {
+	Email   string
+	ZipCode string
+}
+
+func NewPerson() Person {
+	return Person{}
+}
+
+func (p Person) Print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *Person) UpdateFirstName(firstName string, values []string) {
+	p.FirstName = firstName
+	for _, value := range values {
+		p.Values = append(p.Values, value)
+	}
+}
